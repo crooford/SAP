550e78b20d698c8079bcaa4a9e574529b6e8b56e
Carlos Escudero <escuderocarlos23@gmail.com>
2023-11-15 10:59:50 -0500

    redirecciones

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,21 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  redirects: {
+    "/services/puertas-giratorias/": "/producto/puertas-giratorias/",
+    "/services/puertas-corredizas/": "/producto/puertas-corredizas/",
+    "/services/puertas-telescopicas/": "/producto/puertas-telescopicas/",
+    "/services/puertas-batientes/": "/producto/puertas-batientes/",
+    "/services/puertas-plegadizas/": "/producto/puertas-plegadizas/",
+    "/services/torniquetes/": "/producto/torniquetes/",
+    "/services/puertas-de-seguridad/": "/producto/puertas-de-seguridad/",
+    "/services/huella-digital/": "/producto/huella-digital/",
+    "/services/chapas-magneticas/": "/producto/chapas-magneticas/",
+    "/services/teclado-digital-y-lector-de-tarjetas/":
+      "/producto/teclado-digital-y-lector-de-tarjetas",
+  },
 });
