ee1b469b21c6e6d93a2c6cb5dfe18ee7db507ad2
Seozz <105168048+crooford@users.noreply.github.com>
2024-12-23 08:18:02 -0500

    update: astro, add: swiper,fowbite

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,5 +10,5 @@ module.exports = {
     },
     extend: {},
   },
-  plugins: [require("flowbite/plugin"), require("tailwindcss-animated")],
+  plugins: [require('flowbite/plugin'), require("tailwindcss-animated")],
 };
